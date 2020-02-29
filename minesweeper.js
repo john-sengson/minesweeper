a76163ffe2e18af82ea7e11c1ff8c09a27fcca04
john-sengson <j10i_freedom@hotmail.com>
2020-03-01 10:19:09 +1300

    progress with challenge

--- minesweeper.js
+++ minesweeper.js
@@ -2,27 +2,50 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
-  cells: [{row:1, col:1, isMine:[false], hidden:[true]},
-          {row:1.5, col:1.5, isMine:[false], hidden:[true]}, 
-          {row:.5, col:.5, isMine:[false], hidden:[true]}, 
-          {row:0, col:0, isMine:[false], hidden:[true]}]
+  cells: [{row:1, col:1, isMine:[true], hidden:[true]},
+          {row:1.5, col:1.5, isMine:[true], hidden:[true]}, 
+          {row:1.75, col:1.75, isMine:[true], hidden:[true]}, 
+          {row:2, col:2, isMine:[true], hidden:[true]}, 
+          {row:2.5, col:2.5, isMine:[true], hidden:[true]}, 
+          {row:1.25, col:1.25, isMine:[true], hidden:[true]}, 
+          {row:3, col:3, isMine:[true], hidden:[true]}, 
+          {row:.5, col:.5, isMine:[true], hidden:[true]}, 
+          {row:0, col:0, isMine:[true], hidden:[true]}]
   }
 //console.log(board.cells.length); it gave 4 so the row and col numbers should be less than 4 as per the test that row and cell.number should be > length
+// add cells to make it 9 squares
+
 
 
 
 function startGame () {
-
-  
-
-console.log(lib.getSurroundingCells)
-
-
-
-
   // Don't remove this function call: it makes the game work!
+ var countSurroundingMines=[];
+ for (var i= 0; i > board.cells.length; i++); {
+ countSurroundingMines = board.cells[i];
+ console.log(board.cells[i]);
+}
+
+
+
+
+
+
   lib.initBoard()
 }
+
+
+
+
+
+
+
+//board.forEach(function(cells){
+  //cells.surroundingMines() = countSurroundingMines()
+//});
+// error of board.foreach is not a function :(
+
+
 
 // Define this function to look for a win condition:
 //
